4fe68c272801c1bfb66647575ee18ccbff50737b
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-04-25 11:20:54 +0500

    add admin and localize

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
+from ice_cream.models import Category, Topping, Wrapper, IceCream
 
-# Register your models here.
+
+@admin.register(IceCream)
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'is_published',
+        'output_order'
+    )
+    list_editable = (
+        'slug',
+        'is_published',
+        'output_order'
+    )
+    inlines = (
+        IceCreamInline,
+    )
+
+
+@admin.register(Topping)
+class ToppingAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug'
+    )
+    list_editable = (
+        'slug',
+    )
+
+
+@admin.register(Wrapper)
+class WrapperAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+
+
+admin.site.empty_value_display = 'Не задано'
